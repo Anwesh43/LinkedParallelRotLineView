4f2b376e70553b3f76f40ed6936f504c2001bacf
Anwesh43 <anweshthecool0@gmail.com>
2019-02-05 03:43:12 +0530

    adding ParallelRotLineView class

--- parallelrotview/src/main/java/com/anwesh/uiprojects/parallelrotview/ParallelRotLineView.kt
+++ parallelrotview/src/main/java/com/anwesh/uiprojects/parallelrotview/ParallelRotLineView.kt
@@ -56,3 +56,21 @@ fun Canvas.drawPRLNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class ParallelRotLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
