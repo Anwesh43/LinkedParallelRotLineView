29bf6f42bf37c99fe4af0a788a1ac1721352546e
Anwesh43 <anweshthecool0@gmail.com>
2019-02-05 02:49:29 +0530

    adding constants

--- parallelrotview/src/main/java/com/anwesh/uiprojects/parallelrotview/ParallelRotLineView.kt
+++ parallelrotview/src/main/java/com/anwesh/uiprojects/parallelrotview/ParallelRotLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.parallelrotview
 /**
  * Created by anweshmishra on 05/02/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#212121")
