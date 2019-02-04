0712f21a00079d93a723cbd7e6d7ec04c7c0399c
Anwesh43 <anweshthecool0@gmail.com>
2019-02-05 04:06:51 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedparallelrotlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedparallelrotlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedparallelrotlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.parallelrotview.ParallelRotLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ParallelRotLineView.create(this)
     }
 }
