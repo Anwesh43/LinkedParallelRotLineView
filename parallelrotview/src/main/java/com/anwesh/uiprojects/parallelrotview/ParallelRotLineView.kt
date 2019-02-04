681409785dc04594686f45612293bfba86c57195
Anwesh43 <anweshthecool0@gmail.com>
2019-02-05 03:40:34 +0530

    adding drawPRLNode canvas extension method

--- parallelrotview/src/main/java/com/anwesh/uiprojects/parallelrotview/ParallelRotLineView.kt
+++ parallelrotview/src/main/java/com/anwesh/uiprojects/parallelrotview/ParallelRotLineView.kt
@@ -26,4 +26,33 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawPRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = foreColor
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..(lines - 1)) {
+        val scj : Float = sc1.divideScale(j, lines)
+        save()
+        rotate(180f * j)
+        translate(-size, size * scj)
+        for (k in 0..(lines - 1)) {
+            val sck : Float = sc2.divideScale(k, 2)
+            save()
+            rotate(-90f * sck)
+            drawLine(0f, 0f, 2 * size, 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
